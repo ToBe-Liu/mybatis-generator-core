0bac92fa7f4bfdae5d553f88ae191d4b4bc437b3
lxh <1354231662@qq.com>
2018-08-22 13:39:44 +0800

    【FIX】修复 pom文件

---
+++ cmd.bat
@@ -0,0 +1,5 @@
+echo start;
+cd C:\Users\Administrator\.m2\repository\org\mybatis\generator\mybatis-generator-core\1.4.0-SNAPSHOT
+java -jar mybatis-generator-core-1.4.0-SNAPSHOT.jar -configfile generatorConfig.xml -overwrite
+echo end;
+pause;
